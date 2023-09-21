328e3880500708a0fd67cba4a8f1c2f7c17aa70e
vovan-ivanoff <nnyo-yo@yandex.ru>
2023-09-22 00:31:20 +0300

    added conversion beetwen zaycev and normal tu4

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import argparse
 
 
-word = input()[::-1]
-lenta = list(word.rjust(99) + " ")[::-1]
-init_position = len(word)+1
 states = {}
 
 parser = argparse.ArgumentParser(
@@ -11,19 +8,25 @@ parser = argparse.ArgumentParser(
                     description='Python implementation of tu4 interpreter',
                     epilog='madeby vovan_ivanoff')
 parser.add_argument('filename')
+parser.add_argument('-c', '--convert', action='store_true')
+parser.add_argument("-o", "--out", default=".\\out.tu4")
 args = parser.parse_args()
-with open(args.filename, "r", encoding="UTF-8") as f:
-    lines = f.readlines()
-for i in reversed(range(len(lines))):
-    lines[i] = lines[i].split("//")[0].rstrip()
-for line in lines:
-    if len(line) < 1:
-        continue
-    toks = line.split(",")
-    if int(toks[0]) not in states.keys():
-        states[int(toks[0])] = {}
-    states[int(toks[0])] = {**states[int(toks[0])], **{toks[1]: [toks[2],
-                            int(toks[3])]}}
+
+
+def read_states():
+    """Функция чтения файла и составления словаря состояний"""
+    with open(args.filename, "r", encoding="UTF-8") as f:
+        lines = f.readlines()
+    for i in reversed(range(len(lines))):
+        lines[i] = lines[i].split("//")[0].rstrip()
+    for line in lines:
+        if len(line) < 1:
+            continue
+        toks = line.split(",")
+        if int(toks[0]) not in states.keys():
+            states[int(toks[0])] = {}
+        states[int(toks[0])] = {**states[int(toks[0])], **{toks[1]: [toks[2],
+                                int(toks[3])]}}
 
 
 def print_lenta(plenta: list, pposition: int, pstate: int):
@@ -64,4 +67,23 @@ def run_tu4(rlenta: list, rstates: dict, init_pos: int):
         print(f"Состояние {state} не определено!")
 
 
-run_tu4(lenta, states, init_position)
+def export_states(rstates: dict, output_path: str):
+    """Функция экспорта состояний в файл для MT Зайцева"""
+    sorted_state_list = list(rstates.keys())
+    sorted_state_list.sort()
+    with open(output_path, "w", encoding="UTF-8") as f:
+        for key in sorted_state_list:
+            for key_inner in rstates[key]:
+                f.write(f"{key:02},{key_inner},{rstates[key][key_inner][0]},\
+{rstates[key][key_inner][1]:02}\n")
+
+
+if __name__ == "__main__":
+    read_states()
+    if not args.convert:
+        word = input()[::-1]
+        lenta = list(word.rjust(99) + " ")[::-1]
+        init_position = len(word)+1
+        run_tu4(lenta, states, init_position)
+    if args.convert:
+        export_states(states, args.out)
